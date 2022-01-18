2f1148fe8dc3c8087883d2ab1392c876e8a7f9c4
kingqabil <kingqabil@live.com>
2022-01-18 11:22:14 +0300

    Add InputTodo function

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,27 +2,29 @@ import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import "./TodoContainer.css";
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Check linter errors and fix them",
         completed: false,
       },
       {
-        id: 4,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false,
       },
@@ -53,10 +55,22 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
