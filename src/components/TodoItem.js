6fc5b9c15469c6190c9be053f634dc56f21e0d34
kingqabil <kingqabil@live.com>
2022-01-18 11:44:50 +0300

    Add completed style, logic and visuals

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React from 'react';
+import styles from './TodoItem.module.css';
 
-const TodoItem = (props) => (
-  <li>
-    {" "}
-    <input
-      type="checkbox"
-      checked={props.todo.completed}
-      onChange={() => props.handleChangeProps(props.todo.id)}
-    />{" "}
-    {props.todo.title}
-  </li>
-);
+const completedStyle = {
+  fontStyle: 'italic',
+  color: '#595959',
+  opacity: 0.4,
+  textDecoration: 'line-through',
+};
+
+const TodoItem = (props) => {
+  const { completed, id, title } = props.todo;
+  return (
+    <li className={styles.item}>
+      {' '}
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
+      />{' '}
+      <button onClick={() => props.delTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
+    </li>
+  );
+};
 
 export default TodoItem;
