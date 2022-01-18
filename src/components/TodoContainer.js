1a6bf1af7269f073578fa5b5ce0c464e5ad732c8
kingqabil <kingqabil@live.com>
2022-01-18 10:51:02 +0300

    Add Todo components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import TodosList from "./TodosList";
+import Header from "./Header";
+import "./TodoContainer.css";
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Check linter errors and fix them",
+        completed: false,
+      },
+      {
+        id: 4,
+        title: "Deploy to live server",
+        completed: false,
+      },
+    ],
+  };
+
+  handleChange = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+export default TodoContainer;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoItem = (props) => (
+  <li>
+    {" "}
+    <input
+      type="checkbox"
+      checked={props.todo.completed}
+      onChange={() => props.handleChangeProps(props.todo.id)}
+    />{" "}
+    {props.todo.title}
+  </li>
+);
+
+export default TodoItem;
